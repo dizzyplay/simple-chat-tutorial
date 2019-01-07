9958ff21ddf0b457e026613e55f45e7512b1d9af
dizzyplay <vovoboss@gmail.com>
2019-01-08 01:19:59 +0900

    init

---
+++ chat/settings/dev.py
@@ -0,0 +1,14 @@
+from .settings import *
+import os
+
+DEBUG=True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+    },
+}
